cc42ef9bcd5e8748ddbd4bb65c0220b35d915772
anthonyaubert <45994782+anthonyaubert@users.noreply.github.com>
2024-08-18 16:03:50 +0200

    First commit

---
+++ src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const c = @cImport({
+    @cDefine("USE_HAL_DRIVER", {});
+    @cDefine("STM32WB50xx", {});
+    @cDefine("__PROGRAM_START", {}); //bug: https://github.com/ziglang/zig/issues/19687
+    @cInclude("main.h");
+});
+
+export fn zig_entrypoint() void {
+    while (true) {
+        c.HAL_GPIO_WritePin(c.LED_GPIO_Port, c.LED_Pin, c.GPIO_PIN_RESET);
+        c.HAL_Delay(200);
+        c.HAL_GPIO_WritePin(c.LED_GPIO_Port, c.LED_Pin, c.GPIO_PIN_SET);
+        c.HAL_Delay(500);
+    }
+
+    unreachable;
+}
