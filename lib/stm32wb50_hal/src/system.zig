3bac55f95ab848492c235775769cd93facd6a5b8
anthonyaubert <45994782+anthonyaubert@users.noreply.github.com>
2025-02-23 22:21:02 +0100

    Update system with multiple low level functions

--- lib/stm32wb50_hal/src/system.zig
+++ lib/stm32wb50_hal/src/system.zig
@@ -1,11 +1,11 @@
-const c = @cImport({
-    @cDefine("USE_HAL_DRIVER", {});
-    @cDefine("STM32WB50xx", {});
-    @cDefine("__PROGRAM_START", {});
-    @cInclude("main.h");
-});
+const hal = @import("hal.zig");
+const c = hal.c;
+const mmio = @import("mmio.zig");
 
-pub fn getTick() u32 {
+const SCB: *c.SCB_Type = @ptrFromInt(c.SCB_BASE);
+const AIRCR = &SCB.AIRCR;
+
+pub inline fn getTick() u32 {
     return c.HAL_GetTick();
 }
 
@@ -13,10 +13,78 @@ pub inline fn delay(delay_ms: u32) void {
     c.HAL_Delay(delay_ms);
 }
 
-pub fn enterCriticalSection() void {
-    //TODO
+/// Enters a critical section and disables interrupts globally.
+/// Call `.leave()` on the return value to restore the previous state.
+pub inline fn enterCriticalSection() CriticalSection {
+    const section = CriticalSection{
+        ._primask = getPrimask(),
+    };
+    disableInterrupt();
+    return section;
 }
 
-pub fn exitCriticalSection() void {
-    //TODO
+/// A critical section structure that allows restoring the interrupt
+/// status that was set before entering.
+const CriticalSection = struct {
+    _primask: u32,
+
+    /// Leaves the critical section and restores the interrupt state.
+    pub inline fn exit(self: @This()) void {
+        setPrimask(self._primask);
+    }
+};
+
+///Disable Interrupts
+pub inline fn disableInterrupt() void {
+    asm volatile ("CPSID    I");
+}
+
+///Enable Interrupts
+pub inline fn enableInterrupt() void {
+    asm volatile ("CPSIE    I");
+}
+
+pub inline fn getPrimask() u32 {
+    return asm volatile ("mrs r0, PRIMASK"
+        : [ret] "={r0}" (-> u32),
+        :
+        : "r0"
+    );
+}
+
+pub inline fn setPrimask(primask: u32) void {
+    asm volatile ("msr   primask, %[primask]"
+        :
+        : [primask] "r" (primask),
+    );
+}
+
+pub inline fn isIrqMode() bool {
+    return asm volatile ("mrs r0, ipsr"
+        : [ret] "={r0}" (-> bool),
+        :
+        : "r0"
+    );
+}
+
+pub fn softReset() void {
+    //__NVIC_SystemReset - core_cm4.h
+    dsb();
+    mmio.writeReg(AIRCR, (0x5FA << 16) | (AIRCR.* & (7 << 8)) | (1 << 2));
+    isb();
+    while (true) {}
+
+    unreachable;
+}
+
+pub inline fn compilerBarrier() void {
+    asm volatile ("" ::: "memory");
+}
+
+pub inline fn dsb() void {
+    asm volatile ("dsb 0xF" ::: "memory");
+}
+
+pub inline fn isb() void {
+    asm volatile ("isb 0xF" ::: "memory");
 }
